252e02acea073e1072a40dee4caea2d733fc8596
Sahara <sahararod@gmail.com>
2024-06-26 15:54:10 -0600

    se corrigio el codigo, se cambio a ES6, se cambio el uso a promesas

--- src/index.js
+++ src/index.js
@@ -1,13 +1,53 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var A = 'https://rickandmortyapi.com/api/character/';
-var B = new XMLHttpRequest();
+const A = 'https://rickandmortyapi.com/api/character/';
 
-function X(a, b) {
+const X = (url) => {
+  return new Promise((resolve, reject) =>{
+    const B = new XMLHttpRequest();
+    B.onreadystatechange = () =>{
+      if(B.readyState === 4){
+        if(B.status === 200)
+          resolve(JSON.parse(B.responseText));
+        else
+          reject(new Error('Error ' + B.status));
+      }
+    }
+    B.open('GET', url, false);
+    B.send();
+  });  
+}
+
+let d, f;
+
+X(A)
+  .then(data => {
+    d = data;
+    console.log('Primer Llamado...');
+    return X(A + d.results[0].id);
+  })
+  .then(data => {
+    console.log('Segundo Llamado...');
+    f = data;
+    return X(f.origin.url);
+  })
+  .then(data => {
+    console.log('Tercer Llamado...');
+    console.log(`Personajes: ${d.info.count}`);
+    console.log(`Primer Personaje: ${f.name}`);
+    console.log(`Dimensión: ${data.dimension}`);
+  })
+  .catch(error => {
+    console.error('Error: ' + error.message);
+  });
+
+/*function X(a, b) {
+  const B = new XMLHttpRequest();
   B.onreadystatechange = function (e) {
-    if (B.readyState == '4') {
-      if (B.status === '200')
-        b(null, B.responseText);
+    if (B.readyState == 4) {
+      if (B.status === 200)
+        b(null, JSON.parse(B.responseText));
+        //b(null, B.responseText);
       else return b(a);
     }
     else return b(a);
@@ -22,12 +62,17 @@ X(A, function (c, d) {
   X(A + d.results[0].id, function (e, f) {
     if (e) return console.error('Error' + ' ' + e);
     console.log('Segundo Llamado...');
-    X(JSON.parse(f).origin.url, function (g, h) {
+    X(f.origin.url, function (g,h) {
+    //X(JSON.parse(f).origin.url, function (g, h) {
       if (g) return console.error('Error' + ' ' + g);
       console.log('Tercer Llamado...');
-      console.log('Personajes:' + ' ' + JSON.parse(d).info.count);
-      console.log('Primer Personaje:' + ' ' + JSON.parse(f).name);
-      console.log('Dimensión:' + ' ' + JSON.parse(h).dimension);
+      console.log('Lo que hay en d:', d);
+      console.log('Personajes:' + ' ' + d.info.count);
+      //console.log('Personajes:' + ' ' + JSON.parse(d).info.count);
+      console.log('Primer Personaje:' + ' ' + f.name);
+      //console.log('Primer Personaje:' + ' ' + JSON.parse(f).name);
+      console.log('Dimensión:' + ' ' + h.dimension);
+      //console.log('Dimensión:' + ' ' + JSON.parse(h).dimension);
     });
   });
-});
+});*/

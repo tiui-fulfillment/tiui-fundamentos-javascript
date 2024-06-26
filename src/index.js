55e00a13dac3334081ec5e87a6048f31238988b2
AlexMaderaP <alejandro.maderap@gmail.com>
2024-06-26 16:47:09 -0600

    Add: refactorizar callbacks por promesas

--- src/index.js
+++ src/index.js
@@ -3,31 +3,32 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const A = 'https://rickandmortyapi.com/api/character/';
 const B = new XMLHttpRequest;
 
-const X = (a, b) => {
-  B.onreadystatechange = () => {
-    if (B.readyState == '4') {
-      if (B.status === 200)
-        b(null, B.responseText);
-      else return b(a);
-    }
-    else return b(a);
-  };
-  B.open('GET', a, false);
-  B.send();
+const X = (a) => {
+  return new Promise((resolve, reject) => {
+    B.onreadystatechange = () => {
+      if (B.readyState == "4") {
+        if (B.status === 200) resolve(B.responseText);
+      }
+      reject(a);
+    };
+    B.open("GET", a, false);
+    B.send();
+  });
 };
 
-X(A, (c, d) => {
-  if (c) return console.error(`Error ${c}`);
-  console.log('Primer Llamado...');
-  X(A + JSON.parse(d).results[0].id, (e, f) => {
-    if (e) return console.error(`Error ${e}`);
-    console.log('Segundo Llamado...');
-    X(JSON.parse(f).origin.url, (g, h) => {
-      if (g) return console.error(`Error ${g}`);
-      console.log('Tercer Llamado...');
-      console.log(`Personajes: ${JSON.parse(d).info.count}`);
-      console.log(`Primer Personaje: ${JSON.parse(f).name}`);
-      console.log(`Dimensión: ${JSON.parse(h).dimension}`);
-    });
-  });
-});
+X(A)
+  .then((b) => {
+    console.log("Primer Llamado...");
+    return X(A + JSON.parse(b).results[0].id).then((c) => ({ b, c }));
+  })
+  .then(({ b, c }) => {
+    console.log("Segundo LLamado...");
+    return X(JSON.parse(c).origin.url).then((d) => ({ b, c, d }));
+  })
+  .then(({ b, c, d }) => {
+    console.log("Tercer Llamado...");
+    console.log(`Personajes: ${JSON.parse(b).info.count}`);
+    console.log(`Primer Personaje: ${JSON.parse(c).name}`);
+    console.log(`Dimensión: ${JSON.parse(d).dimension}`);
+  })
+  .catch((error) => console.log(`Error: ${error}`));

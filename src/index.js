3268b12c54225e4f3910e28a38781e4ac88c5fc7
MonkiG <raan.heam@gmail.com>
2024-06-26 13:26:59 -0600

    add: fix code to work correctly

--- src/index.js
+++ src/index.js
@@ -1,33 +1,56 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var A = 'https://rickandmortyapi.com/api/character/';
-var B = new XMLHttpRequest();
+const API_URL = 'https://rickandmortyapi.com/api/character/';
 
-function X(a, b) {
-  B.onreadystatechange = function (e) {
-    if (B.readyState == '4') {
-      if (B.status === '200')
-        b(null, B.responseText);
-      else return b(a);
+function getData(url, callback) {
+  const xml = new XMLHttpRequest();
+
+  xml.onreadystatechange = function (e) {
+    if (xml.readyState === 4) {
+      if (xml.status === 200){
+        console.log('entro acá')
+        const reponseParsed = JSON.parse(xml.responseText)
+        callback(null, reponseParsed);
+      }
+      else {
+        return callback(new Error('Request failed with status ' + xml.status))
+      };
     }
-    else return b(a);
+    else {
+     
+      return callback(new Error('Request failed with status ' + xml.status))
+    };
   };
-  B.open('GET', a, false);
-  B.send();
+
+  xml.onerror = function (e) {
+    callback(new Error(`Request failed due to an error: ${e}`))
+  };
+
+  xml.open('GET', url, true);
+  xml.send();
+
 };
 
-X(A, function (c, d) {
-  if (c) return console.error('Error' + ' ' + c);
+getData(API_URL, function (error, data) {
+  if (error) {
+    return console.error('Error' + ' ' + error)
+  };
+  
   console.log('Primer Llamado...');
-  X(A + d.results[0].id, function (e, f) {
-    if (e) return console.error('Error' + ' ' + e);
+
+  getData(API_URL + data.results[0].id, function (error2, f) {
+   
+    if (error2) return console.error('Error2' + ' ' + error2);
+    
     console.log('Segundo Llamado...');
-    X(JSON.parse(f).origin.url, function (g, h) {
-      if (g) return console.error('Error' + ' ' + g);
+    
+    getData(f.origin.url, function (error3, h) {
+      if (error3) return console.error('Error' + ' ' + error3);
       console.log('Tercer Llamado...');
-      console.log('Personajes:' + ' ' + JSON.parse(d).info.count);
-      console.log('Primer Personaje:' + ' ' + JSON.parse(f).name);
-      console.log('Dimensión:' + ' ' + JSON.parse(h).dimension);
+      console.log('Personajes:' + ' ' + data.info.count);
+      console.log('Primer Personaje:' + ' ' + f.name);
+      console.log('Dimensión:' + ' ' + h.dimension);
+    
     });
   });
 });

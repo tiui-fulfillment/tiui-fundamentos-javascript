84a30f5c6e28e13c2ac1e98372336073ddf754cb
marcknova <marck.nova@gmail.com>
2024-06-27 17:36:30 -0600

    Implementación de la solución para la prueba técnica primera solucion

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -1,20 +1,23 @@
 ## DESCRIPTION
 
-Nombre:
-Email:
-WhatsApp:
-Ciudad:
-Pais:
+Nombre: Marco Antonio Monterrosas Temoxtle
+Email: marcoa.mot97@gmail.com
+WhatsApp: 2722085138
+Ciudad: Veracruz
+Pais: Orizaba
 
 # Retos:
-  - [ ] Primer problema
-  - [ ] Segundo problema
-  - [ ] Tercer problema
-  - [ ] Cuarto problema
-  - [ ] Quinto problema
-  - [ ] Opcional
+  - [x] Primer problema
+  - [x] Segundo problema
+  - [x] Tercer problema
+  - [x] Cuarto problema
+  - [x] Quinto problema
+  - [x] Opcional
 
 ## ¿Como explicarias el proposito de este proyecto? (opcional)
 
+Este proyecto tiene como propósito demostrar mi capacidad para desarrollar aplicaciones fullstack utilizando tecnologías modernas. Utilicé la API de Rick and Morty para mostrar cómo integrar y manejar datos externos, habilidades esenciales para crear sistemas de seguimiento de envíos en tiempo real y interfaces intuitivas para usuarios y equipos de logística en una empresa de envíos.
 
 ## ¿Cuál es tu stack tecnológico preferido? ¿Por qué? (opcional)
+
+No tengo uno preferido si bien me siento comodo trabajando con react js y node js(express) siempre estoy abierto a aprender nuevo stack
--- src/index.js
+++ src/index.js
@@ -1,33 +1,39 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+/* Primer problema modificacion y correccion de errores */
 
-var A = 'https://rickandmortyapi.com/api/character/';
-var B = new XMLHttpRequest();
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var A = "https://rickandmortyapi.com/api/character/";
 
 function X(a, b) {
+  var B = new XMLHttpRequest(); // Creamos nueva instancia en cada llamada
   B.onreadystatechange = function (e) {
-    if (B.readyState == '4') {
-      if (B.status === '200')
-        b(null, B.responseText);
-      else return b(a);
+    if (B.readyState === 4) {
+      if (B.status === 200) {
+        b(null, JSON.parse(B.responseText)); // Parseamos la respuesta JSON
+      } else {
+        b(a); // Redundancia en el uso del return
+      }
     }
-    else return b(a);
   };
-  B.open('GET', a, false);
+  B.open("GET", a, true); // hacemos uso de una solicitud asíncrona
   B.send();
-};
+}
 
 X(A, function (c, d) {
-  if (c) return console.error('Error' + ' ' + c);
-  console.log('Primer Llamado...');
+  if (c) return console.error("Error primero: " + c);
+  console.log("Primer Llamado...");
+
   X(A + d.results[0].id, function (e, f) {
-    if (e) return console.error('Error' + ' ' + e);
-    console.log('Segundo Llamado...');
-    X(JSON.parse(f).origin.url, function (g, h) {
-      if (g) return console.error('Error' + ' ' + g);
-      console.log('Tercer Llamado...');
-      console.log('Personajes:' + ' ' + JSON.parse(d).info.count);
-      console.log('Primer Personaje:' + ' ' + JSON.parse(f).name);
-      console.log('Dimensión:' + ' ' + JSON.parse(h).dimension);
+    if (e) return console.error("Error segundo: " + e);
+    console.log("Segundo Llamado...");
+
+    X(f.origin.url, function (g, h) {
+      //quitamos el parseo de aqui
+      if (g) return console.error("Error tercero: " + g);
+      console.log("Tercer Llamado...");
+
+      console.log("Personajes: " + d.info.count);
+      console.log("Primer Personaje: " + f.name);
+      console.log("Dimensión: " + h.dimension);
     });
   });
 });

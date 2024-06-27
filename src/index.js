a3c9eb86066e6c78f14db79ebefd843614fe55cb
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2024-06-26 23:10:23 -0600

    feat: add promises to fetch data (Tercer Problema)
    Promises and two ways to obtain data were added: fetch and XMLHttpRequest

--- src/index.js
+++ src/index.js
@@ -1,34 +1,58 @@
-import { XMLHttpRequest } from 'xmlhttprequest'
+import { XMLHttpRequest } from 'xmlhttprequest';
 
 const A = 'https://rickandmortyapi.com/api/character/';
 const B = new XMLHttpRequest();
 
+/*
+// Use this function to get the data from an API using fetch
+
+const X = async (a, b) => {
+  const r = await fetch(a);
+  const d = await r.text();
+  b(d);
+  return d;
+}
+*/
+
+// Use this function to get the data from an API using XMLHttpRequest
 function X(a, b) {
-  B.onreadystatechange = (e) => {
-    if (B.readyState == '4') {
-      if (B.status == '200') // ok - he == operator does the type conversion of the operands before comparison
-        b(null, B.responseText);
+  return new Promise((c) => {
+    B.onreadystatechange = function (e) {
+      if (B.readyState == '4') {
+        if (B.status == '200')
+          b(null, B.responseText);
+        else return b(a);
+      }
       else return b(a);
-    }
-    else return b(a);
-  };
-  B.open('GET', a, false);
-  B.send();
-};
+    };
+    B.open('GET', a, false);
+    B.send();
 
-
-X(A, (c, d) => {
-  if (c) return console.error(`Error ${c}`);
-  console.log('Primer Llamado...');
-  X(A + JSON.parse(d).results[0].id, (e, f) => { // JSON.parse(d) is an object, so we can access its properties with the dot notation
-    if (e) return console.error(`Error ${e}`);
-    console.log('Segundo Llamado...');
-    X(JSON.parse(f).origin.url, (g, h) => {
-      if (g) return console.error(`Error ${g}`);
-      console.log('Tercer Llamado...');
-      console.log(`Personajes: ${JSON.parse(d).info.count}`);
-      console.log(`Primer Personaje: ${JSON.parse(f).name}`);
-      console.log(`Dimensión: ${JSON.parse(h).dimension}`);
-    });
+    return c(B.responseText);
   });
-});
+}
+
+const R = async () => {
+  try {
+    const r1 = await X(A, () => {
+      console.log('Primer Llamado...');
+    });
+
+    const r2 = await X(A + JSON.parse(r1).results[0].id, () => {
+      console.log('Segundo Llamado...');
+    });
+
+    const r3 = await X(JSON.parse(r2).origin.url, () => {
+      console.log('Tercer Llamado...');
+    });
+
+    console.log(`Personajes: ${JSON.parse(r1).info.count}`);
+    console.log(`Primer Personaje: ${JSON.parse(r2).name}`);
+    console.log(`Dimensión: ${JSON.parse(r3).dimension}`);
+
+  } catch (e) {
+    console.error(`Error ${e}`);
+  }
+}
+
+R();

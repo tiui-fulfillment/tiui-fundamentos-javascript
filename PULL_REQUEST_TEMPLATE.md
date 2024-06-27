72232eb2c75f423806700402b14bed00fe914b38
Brandz4 <branyoz458@outlook.com>
2024-06-27 04:48:51 -0600

    5 problemas resueltos

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -1,10 +1,10 @@
 ## DESCRIPTION
 
-Nombre:
-Email:
-WhatsApp:
-Ciudad:
-Pais:
+Nombre: Brandon Yahir Osorio Zamudio
+Email: branyoz458@gmail.com
+WhatsApp: 442-556-3637
+Ciudad: Querétaro, Querétaro.
+Pais: México.
 
 # Retos:
   - [ ] Primer problema
@@ -14,7 +14,6 @@ Pais:
   - [ ] Quinto problema
   - [ ] Opcional
 
-## ¿Como explicarias el proposito de este proyecto? (opcional)
+## ¿Como explicarias el proposito de este proyecto? Tiene como objetivo implementar soluciones a problemas prácticos y típicos como; corregir errores, modernizar el código con ECMAScript 6 y Promesas, y optimizar la eficiencia y legibilidad. 
 
-
-## ¿Cuál es tu stack tecnológico preferido? ¿Por qué? (opcional)
+## ¿Cuál es tu stack tecnológico preferido? ¿Por qué? Mi stack tecnológico preferido incluye JavaScript como lenguaje principal, con Node.js para el backend, mysql para base de datos y React para el frontend. JavaScript es versátil y ampliamente utilizado, lo que facilita el desarrollo de aplicaciones completas. Node.js proporciona un entorno de ejecución eficiente para el backend, mientras que React es mi elección para construir interfaces de usuario dinámicas y escalables debido a su eficiencia en el renderizado y su arquitectura basada en componentes. Recientemente en las últimas semanas he comenzado a prender React debido a su integración con JS y Node.JS con los cuales ya he trabajado anteriormente.
--- src/index.js
+++ src/index.js
@@ -1,33 +1,53 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var A = 'https://rickandmortyapi.com/api/character/';
-var B = new XMLHttpRequest();
+const API = 'https://rickandmortyapi.com/api/character/';
+const xhttp = new XMLHttpRequest();
 
-function X(a, b) {
-  B.onreadystatechange = function (e) {
-    if (B.readyState == '4') {
-      if (B.status === '200')
-        b(null, B.responseText);
-      else return b(a);
-    }
-    else return b(a);
-  };
-  B.open('GET', a, false);
-  B.send();
+const fetchData = (url_api) => {
+  return new Promise((resolve, reject) => {
+    xhttp.onreadystatechange = () => {
+      if (xhttp.readyState === 4) {
+        if (xhttp.status === 200)
+          resolve(xhttp.responseText);
+        else
+          reject(url_api);
+      }
+      else
+        reject(url_api);
+    };
+    xhttp.open('GET', url_api, false);
+    xhttp.send();
+  });
 };
 
-X(A, function (c, d) {
-  if (c) return console.error('Error' + ' ' + c);
-  console.log('Primer Llamado...');
-  X(A + d.results[0].id, function (e, f) {
-    if (e) return console.error('Error' + ' ' + e);
+let data1, data2; // Variables para almacenar los datos recibidos en los primeros dos llamados
+
+fetchData(API)
+  .then(response => {
+    data1 = JSON.parse(response);
+    console.log('Primer Llamado...');
+    return fetchData(`${API}${data1.results[0].id}`);
+  })
+  .then(response => {
+    data2 = JSON.parse(response);
     console.log('Segundo Llamado...');
-    X(JSON.parse(f).origin.url, function (g, h) {
-      if (g) return console.error('Error' + ' ' + g);
-      console.log('Tercer Llamado...');
-      console.log('Personajes:' + ' ' + JSON.parse(d).info.count);
-      console.log('Primer Personaje:' + ' ' + JSON.parse(f).name);
-      console.log('Dimensión:' + ' ' + JSON.parse(h).dimension);
-    });
-  });
-});
+    return fetchData(data2.origin.url);
+  })
+  .then(response => {
+    const data3 = JSON.parse(response);
+    console.log('Tercer Llamado...');
+    console.log(`Personajes: ${data1.info.count}`);
+    console.log(`Primer Personaje: ${data2.name}`);
+    console.log(`Dimensión: ${data3.dimension}`);
+  })
+  .catch(error => console.error(`Error ${error}`));
+
+/*
+  Problema 1, importación correcta de XMLHttpRequest.
+  Problema 2, se implementó la estructura indicada.
+  Problema 3, Se reescribió la función fetchData utilizando promesas en lugar de callbacks, evitando el Callback Hell.
+  Problema 4, mejora en la estructura y funcionamiento del codigo y corrección en el acceso a las propiedades de los datos parseados en cada paso.
+*/
+
+//Nota, trate de pasarlo a TypeScript pero como realmente no tengo experiencia con ese lenguaje tuve algunas complicaciones pero desde ya comenzaré
+// a familiarizarme con su funcionamiento. 

fd0786da6e4909442642b7271670f289e741f90c
jesusrobot0 <jesusrobot9000@icloud.com>
2024-06-27 15:20:57 -0500

    Solución al tercer problema - Ahora no hay callback hell gracias al uso de promesas

--- src/index.js
+++ src/index.js
@@ -1,40 +1,46 @@
 import { XMLHttpRequest } from "xmlhttprequest";
 
 const A = 'https://rickandmortyapi.com/api/character/';
-const B = new XMLHttpRequest(); 
 
-const X = (a, b) => {
-  B.onreadystatechange = (e) => {
-    if (B.readyState == '4') {
-      if (B.status === 200) {
-        try {
-          const parsedResponse = JSON.parse(B.responseText)
-          b(null, parsedResponse);
-        } catch (error) {
-          b(new Error('Error parsing response'));
+const X = (a) => {
+  return new Promise((resolve, reject) => {
+  const B = new XMLHttpRequest(); 
+    B.onreadystatechange = (e) => {
+      if (B.readyState == '4') {
+        if (B.status === 200) {
+          try {
+            const parsedResponse = JSON.parse(B.responseText)
+            resolve(parsedResponse)
+          } catch (error) {
+            reject(new Error('Error parsing response'))
+          }
+        } else {
+          reject(new Error(`HTTP error ${B.status}`));
         }
-      } else {
-        return b(a);
       }
-    }
-    else return b(a);
-  };
-  B.open('GET', a, false);
-  B.send();
+    };
+
+    B.onerror = () => {
+      reject(new Error("Network error"));
+    };
+    B.open('GET', a, true);
+    B.send();
+  })
 };
 
-X(A,  (c, d) => {
-  if (c) return console.error(`Error ${c}`);
-  console.log('Primer Llamado...');
-  X(A + d.results[0].id,  (e, f) => {
-    if (e) return console.error(`Error ${e}`);
+X(A)
+  .then(d => {
+    console.log('Primer Llamado...');
+    return X(`${A}${d.results[0].id}`).then(f => ({ d, f }));
+  })
+  .then(({ d, f }) => {
     console.log('Segundo Llamado...');
-    X(f.origin.url,  (g, h) => {
-      if (g) return console.error(`Error ${g}`);
-      console.log('Tercer Llamado...');
-      console.log(`Personajes: ${d.info.count}`);
-      console.log(`Primer Personaje: ${f.name}`);
-      console.log(`Dimensión: ${h.dimension}`);
-    });
-  });
-});
+    return X(f.origin.url).then(g => ({ d, f, g }));
+  })
+  .then(({ d, f, g }) => {
+    console.log('Tercer Llamado...');
+    console.log(`Personajes: ${d.info.count}`);
+    console.log(`Primer Personaje: ${f.name}`);
+    console.log(`Dimensión: ${g.dimension}`);
+  })
+  .catch(c => console.error(c));

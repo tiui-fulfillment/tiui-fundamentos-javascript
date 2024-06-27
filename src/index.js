b611cb1f2a85c4f9036ec9cda629f98fb03c45a9
jesusrobot0 <jesusrobot9000@icloud.com>
2024-06-27 14:21:23 -0500

    Solución al segundo problema - Ahora el código de la app esta utilizando algunas de las caracteristícas más recientes de ES

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+import { XMLHttpRequest } from "xmlhttprequest";
 
-var A = 'https://rickandmortyapi.com/api/character/';
-var B = new XMLHttpRequest();
+const A = 'https://rickandmortyapi.com/api/character/';
+const B = new XMLHttpRequest(); 
 
-function X(a, b) {
-  B.onreadystatechange = function (e) {
+const X = (a, b) => {
+  B.onreadystatechange = (e) => {
     if (B.readyState == '4') {
       if (B.status === 200) {
         try {
           const parsedResponse = JSON.parse(B.responseText)
           b(null, parsedResponse);
         } catch (error) {
-          b(new Error("Error parsing response"));
+          b(new Error('Error parsing response'));
         }
       } else {
         return b(a);
@@ -23,18 +23,18 @@ function X(a, b) {
   B.send();
 };
 
-X(A, function (c, d) {
-  if (c) return console.error('Error' + ' ' + c);
+X(A,  (c, d) => {
+  if (c) return console.error(`Error ${c}`);
   console.log('Primer Llamado...');
-  X(A + d.results[0].id, function (e, f) {
-    if (e) return console.error('Error' + ' ' + e);
+  X(A + d.results[0].id,  (e, f) => {
+    if (e) return console.error(`Error ${e}`);
     console.log('Segundo Llamado...');
-    X(f.origin.url, function (g, h) {
-      if (g) return console.error('Error' + ' ' + g);
+    X(f.origin.url,  (g, h) => {
+      if (g) return console.error(`Error ${g}`);
       console.log('Tercer Llamado...');
-      console.log('Personajes:' + ' ' + d.info.count);
-      console.log('Primer Personaje:' + ' ' + f.name);
-      console.log('Dimensión:' + ' ' + h.dimension);
+      console.log(`Personajes: ${d.info.count}`);
+      console.log(`Primer Personaje: ${f.name}`);
+      console.log(`Dimensión: ${h.dimension}`);
     });
   });
 });

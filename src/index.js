8239a6d4cba2fcdcda17a31714e412dff9b1165c
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2024-06-26 21:03:42 -0600

    refactor: update to ES6 (Segundo Problema)
    refactoring code to esm6 features:
    - Arrow Functions
    - TemplateStrings

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+import { XMLHttpRequest } from 'xmlhttprequest'
 
-var A = 'https://rickandmortyapi.com/api/character/';
-var B = new XMLHttpRequest();
+const A = 'https://rickandmortyapi.com/api/character/';
+const B = new XMLHttpRequest();
 
 function X(a, b) {
-  B.onreadystatechange = function (e) {
+  B.onreadystatechange = (e) => {
     if (B.readyState == '4') {
       if (B.status == '200') // ok - he == operator does the type conversion of the operands before comparison
         b(null, B.responseText);
@@ -16,18 +16,19 @@ function X(a, b) {
   B.send();
 };
 
-X(A, function (c, d) {
-  if (c) return console.error('Error' + ' ' + c);
+
+X(A, (c, d) => {
+  if (c) return console.error(`Error ${c}`);
   console.log('Primer Llamado...');
-  X(A + JSON.parse(d).results[0].id, function (e, f) { // JSON.parse(d) is an object, so we can access its properties with the dot notation
-    if (e) return console.error('Error' + ' ' + e);
+  X(A + JSON.parse(d).results[0].id, (e, f) => { // JSON.parse(d) is an object, so we can access its properties with the dot notation
+    if (e) return console.error(`Error ${e}`);
     console.log('Segundo Llamado...');
-    X(JSON.parse(f).origin.url, function (g, h) {
-      if (g) return console.error('Error' + ' ' + g);
+    X(JSON.parse(f).origin.url, (g, h) => {
+      if (g) return console.error(`Error ${g}`);
       console.log('Tercer Llamado...');
-      console.log('Personajes:' + ' ' + JSON.parse(d).info.count);
-      console.log('Primer Personaje:' + ' ' + JSON.parse(f).name);
-      console.log('Dimensión:' + ' ' + JSON.parse(h).dimension);
+      console.log(`Personajes: ${JSON.parse(d).info.count}`);
+      console.log(`Primer Personaje: ${JSON.parse(f).name}`);
+      console.log(`Dimensión: ${JSON.parse(h).dimension}`);
     });
   });
 });

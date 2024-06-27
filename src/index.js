d9dd1bffd3248ee595dcd821039dd80afbb3e04c
YeyoM <yeyomoreno2003@hotmail.com>
2024-06-26 18:09:10 -0600

    fix: :fire: Segundo Problema, Arrow Functions y Template Strings

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var A = "https://rickandmortyapi.com/api/character/";
-var B = new XMLHttpRequest();
+const A = "https://rickandmortyapi.com/api/character/";
+const B = new XMLHttpRequest();
 
-function X(a, b) {
-  B.onreadystatechange = function (e) {
-    if (B.readyState == "4") {
-      if (B.status == "200") {
+const X = (a, b) => {
+  B.onreadystatechange = (e) => {
+    if (B.readyState === 4) {
+      if (B.status === 200) {
         b(null, B.responseText);
       } else return b(a);
     } else return b(a);
   };
   B.open("GET", a, false);
   B.send();
-}
+};
 
-X(A, function (c, d) {
-  console.log(c, d);
-  if (c) return console.error("Error" + " " + c);
+X(A, (c, d) => {
+  if (c) return console.error(`Error ${c}`);
   console.log("Primer Llamado...");
   const data = JSON.parse(d);
-  X(A + data.results[0].id, function (e, f) {
-    if (e) return console.error("Error" + " " + e);
+  X(A + data.results[0].id, (e, f) => {
+    if (e) return console.error(`Error ${e}`);
     console.log("Segundo Llamado...");
-    X(JSON.parse(f).origin.url, function (g, h) {
-      if (g) return console.error("Error" + " " + g);
+    X(JSON.parse(f).origin.url, (g, h) => {
+      if (g) return console.error(`Error ${g}`);
       console.log("Tercer Llamado...");
-      console.log("Personajes:" + " " + JSON.parse(d).info.count);
-      console.log("Primer Personaje:" + " " + JSON.parse(f).name);
-      console.log("Dimensión:" + " " + JSON.parse(h).dimension);
+      console.log(`Personajes: ${JSON.parse(d).info.count}`);
+      console.log(`Primer Personaje: ${JSON.parse(f).name}`);
+      console.log(`Dimensión: ${JSON.parse(h).dimension}`);
     });
   });
 });

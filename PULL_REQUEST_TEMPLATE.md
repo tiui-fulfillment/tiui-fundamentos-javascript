10ea869734323cea2e501ba72011ee7534809601
Gabriel Colmenares R <osogamer24@gmail.com>
2024-06-26 23:43:39 -0600

    Solucionado tiui-fundamentos-javascript

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -1,20 +1,25 @@
 ## DESCRIPTION
 
-Nombre:
-Email:
-WhatsApp:
-Ciudad:
-Pais:
+Nombre:   Gabriel Colmenares Rangel
+Email:    gcolmenaresr2001@gmail.com
+WhatsApp: +52 3173887075
+Ciudad:   Autlán de Navarro, Jalisco
+Pais:     México
 
 # Retos:
-  - [ ] Primer problema
-  - [ ] Segundo problema
-  - [ ] Tercer problema
-  - [ ] Cuarto problema
-  - [ ] Quinto problema
-  - [ ] Opcional
+  - [✔] Primer problema
+  - [✔] Segundo problema
+  - [✔] Tercer problema
+  - [✔] Cuarto problema
+  - [✔] Quinto problema
+  - [🤏] Opcional
 
 ## ¿Como explicarias el proposito de este proyecto? (opcional)
 
+En lo personal creo que el propósito de este proyecto es evaluar nuestra capacidad de reconocer errores,
+refactorizar, optimizar y mejorar código ya escrito adicional a la lógica de programación.
 
 ## ¿Cuál es tu stack tecnológico preferido? ¿Por qué? (opcional)
+
+Mi stack preferido o donde me siento más cómodo es usando Python (Flask) como backend, bases de datos relacionales como postgresql y para frontend Angular. Pero también me siento cómodo utilizando NestJS
+en el backend
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var A = 'https://rickandmortyapi.com/api/character/';
 var B = new XMLHttpRequest();
 
+/*
+======  PRIMER PROBLEMA  ======
+*/
+console.log("\n-----  Primer problema -----\n")
 function X(a, b) {
   B.onreadystatechange = function (e) {
-    if (B.readyState == '4') {
-      if (B.status === '200')
+    if (B.readyState == 4) {
+      if (B.status === 200){
         b(null, B.responseText);
-      else return b(a);
+      }else return b(a);
     }
     else return b(a);
   };
@@ -17,13 +21,13 @@ function X(a, b) {
 };
 
 X(A, function (c, d) {
-  if (c) return console.error('Error' + ' ' + c);
+  if (c) return console.error('Error 1' + ' ' + c);
   console.log('Primer Llamado...');
-  X(A + d.results[0].id, function (e, f) {
-    if (e) return console.error('Error' + ' ' + e);
+  X(A + JSON.parse(d).results[0].id, function (e, f) {
+    if (e) return console.error('Error 2' + ' ' + e);
     console.log('Segundo Llamado...');
     X(JSON.parse(f).origin.url, function (g, h) {
-      if (g) return console.error('Error' + ' ' + g);
+      if (g) return console.error('Error 3' + ' ' + g);
       console.log('Tercer Llamado...');
       console.log('Personajes:' + ' ' + JSON.parse(d).info.count);
       console.log('Primer Personaje:' + ' ' + JSON.parse(f).name);
@@ -31,3 +35,72 @@ X(A, function (c, d) {
     });
   });
 });
+
+/*
+======  SEGUNDO PROBLEMA  ======
+*/
+
+console.log("\n-----  Segundo problema -----\n")
+
+function X2(a, b) {
+  B.onreadystatechange = (e) => {
+    B.readyState == 4 ? B.status === 200 ? b(null, B.responseText) : b(a) : b(a);
+  };
+  B.open('GET', a, false);
+  B.send();
+};
+
+X2(A, (c, d) => {
+  c ? console.error(`Error 1 ${c}`) : console.log(`Primer Llamado...`);
+  X2(A + JSON.parse(d).results[0].id, (e, f) =>{
+    e ? console.error(`Error 2 ${e}`) : console.log(`Segundo Llamado...`);
+    X2(JSON.parse(f).origin.url, (g, h) =>{
+      g ? console.error(`Error 3 ${g}`) : 
+      console.log(`Tercer Llamado...`);
+      console.log(`Personajes: ${JSON.parse(d).info.count}`);
+      console.log(`Primer Personaje: ${JSON.parse(f).name}`);
+      console.log(`Dimensión: ${JSON.parse(h).dimension}`);
+    })
+  })
+});
+
+
+/*
+======  TERCER/CUARTO PROBLEMA  ======
+*/
+
+console.log("\n-----  Tercer/cuarto problema -----\n")
+
+function X3(a) {
+  return new Promise((resolve, reject) => {
+    B.onreadystatechange = () => {
+      B.readyState == 4 && B.status === 200 
+      ? resolve(B.responseText) 
+      : reject(new Error(`Hubo un error al procesar la solicitud: ${B.status}`));
+    };
+  B.open('GET', a, false);
+  B.send();
+  })
+};
+
+X3(A)
+  .then(r => {
+    d = JSON.parse(r);
+    console.log(`Primer Llamado...`);
+    return X3(A + d.results[0].id);
+  })
+  .then(r => {
+    f = JSON.parse(r);
+    console.log(`Segundo Llamado...`);
+    return X3(f.origin.url);
+  })
+  .then(r => {
+    h = JSON.parse(r);
+    console.log(`Tercer Llamado...`);
+    console.log(`Personajes: ${d.info.count}`);
+    console.log(`Primer Personaje: ${f.name}`);
+    console.log(`Dimensión: ${h.dimension}`);
+  })
+  .catch(error => {
+    console.error(`Error: ${error.message}`);
+  });

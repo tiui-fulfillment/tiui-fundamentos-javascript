f02b14bbf7cfd6d7f05b86c226026da28b519e90
José Manuel Varela Hernández <josemanuelvarela36@gmail.com>
2024-06-27 01:33:53 -0600

    Segundo ejercicio resuelto

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const url = "https://rickandmortyapi.com/api/character/";
 
-var A = 'https://rickandmortyapi.com/api/character/';
-var B = new XMLHttpRequest();
-
-function X(a, b) {
-  B.onreadystatechange = function (e) {
-    if (B.readyState == '4') {
-      if (B.status === '200')
-        b(null, B.responseText);
-      else return b(a);
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    else return b(a);
-  };
-  B.open('GET', a, false);
-  B.send();
-};
+    return await response.json();
+  } catch (error) {
+    throw new Error(`Fetch error: ${error.message}`);
+  }
+}
 
-X(A, function (c, d) {
-  if (c) return console.error('Error' + ' ' + c);
-  console.log('Primer Llamado...');
-  X(A + d.results[0].id, function (e, f) {
-    if (e) return console.error('Error' + ' ' + e);
-    console.log('Segundo Llamado...');
-    X(JSON.parse(f).origin.url, function (g, h) {
-      if (g) return console.error('Error' + ' ' + g);
-      console.log('Tercer Llamado...');
-      console.log('Personajes:' + ' ' + JSON.parse(d).info.count);
-      console.log('Primer Personaje:' + ' ' + JSON.parse(f).name);
-      console.log('Dimensión:' + ' ' + JSON.parse(h).dimension);
-    });
-  });
-});
+async function getPeticion(url) {
+  try {
+    console.log("Primer Llamado...");
+    const resultado = await fetchData(url);
+
+    console.log("Segundo Llamado...");
+    const resultado2 = await fetchData(url + resultado.results[0].id);
+
+    console.log("Tercer Llamado...");
+    const resultado3 = await fetchData(JSON.parse(resultado2).origin.url);
+
+    console.log(`
+      Personajes: ${resultado.info.count}
+      Primer Personaje: ${resultado2.name}
+      Dimensión: ${resultado3.dimension}
+    `);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+getPeticion(url);

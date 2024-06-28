8c684bf39e3cb3f4b43227fc4d7614117b3cc87c
abner-silva-dev <dylanabner14@gmail.com>
2024-06-27 22:18:25 -0600

    Primer problema

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 var A = 'https://rickandmortyapi.com/api/character/';
 var B = new XMLHttpRequest();
@@ -6,15 +6,14 @@ var B = new XMLHttpRequest();
 function X(a, b) {
   B.onreadystatechange = function (e) {
     if (B.readyState == '4') {
-      if (B.status === '200')
-        b(null, B.responseText);
-      else return b(a);
-    }
-    else return b(a);
+      if (B.status === 200) {
+        b(null, JSON.parse(B.responseText));
+      } else return b(a);
+    } else return b(a);
   };
   B.open('GET', a, false);
   B.send();
-};
+}
 
 X(A, function (c, d) {
   if (c) return console.error('Error' + ' ' + c);
@@ -22,12 +21,12 @@ X(A, function (c, d) {
   X(A + d.results[0].id, function (e, f) {
     if (e) return console.error('Error' + ' ' + e);
     console.log('Segundo Llamado...');
-    X(JSON.parse(f).origin.url, function (g, h) {
+    X(f.origin.url, function (g, h) {
       if (g) return console.error('Error' + ' ' + g);
       console.log('Tercer Llamado...');
-      console.log('Personajes:' + ' ' + JSON.parse(d).info.count);
-      console.log('Primer Personaje:' + ' ' + JSON.parse(f).name);
-      console.log('Dimensión:' + ' ' + JSON.parse(h).dimension);
+      console.log('Personajes:' + ' ' + d.info.count);
+      console.log('Primer Personaje:' + ' ' + f.name);
+      console.log('Dimensión:' + ' ' + h.dimension);
     });
   });
 });

c8cd1b2a77eafbcd5451ab8b12c88a73f3f85abc
Samir Priego <samir.adevs@gmail.com>
2024-06-29 01:06:29 -0600

    4.0
    Problema 4 resuelto: Mejora de efectividad y claridad

--- src/index.js
+++ src/index.js
@@ -1,38 +1,48 @@
 import { XMLHttpRequest } from "xmlhttprequest";
 //var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var A = 'https://rickandmortyapi.com/api/character/';
-var B = new XMLHttpRequest();
+const API = 'https://rickandmortyapi.com/api/character/';
+var xhttp = new XMLHttpRequest();
 
-function X(a) {
+async function fetchData(url_api, callback){
   return new Promise((resolve, reject)=>{
-    B.onreadystatechange = function (e) {
-      if (B.readyState === 4) {
-        if (B.status === 200)
-          resolve(B.responseText);
-        else reject(a);
+    xhttp.onreadystatechange = function (e) {
+      if (xhttp.readyState === 4) {
+        if (xhttp.status === 200)
+          resolve(callback(xhttp.responseText));
+        else reject(new Error(url_api));
       }
-      else reject(a);
+      else reject(new Error(url_api));
     };
-    B.open('GET', a, false);
-    B.send();;
+    xhttp.open('GET', url_api, false);
+    xhttp.send();;
   })
 };
 
 
-X(A).then(d=>{
-  console.log('Primer Llamado...');
-  return X(A + JSON.parse(d).results[0].id)
-    .then((f=>({f, d})))
-    .catch(c=>console.error(`Error  ${c}`))
-}).then(({f, d})=>{
-  console.log('Segundo Llamado...');
-  return X(JSON.parse(f).origin.url)
-    .then((h=>({h, f, d})))
-    .catch(e=>console.error(`Error  ${e}`))
-}).then(({h, f, d})=>{
-  console.log('Tercer Llamado...');
-  console.log(`Personajes: ${JSON.parse(d).info.count}`);
-  console.log(`Primer Personaje: ${JSON.parse(f).name}`);
-  console.log(`Dimensión: ${JSON.parse(h).dimension}`);
-}).catch(g=>console.error(`Error  ${g}`))
+try {
+  await fetchData(API, async data1=>{
+      console.log('Primer Llamado...');
+      const data2 = await fetchData(API + JSON.parse(data1).results[0].id, async data=>{
+        try {
+          return data
+        } catch (error2) {
+          console.error(`Error  ${error2}`)     
+        }
+      })
+      console.log('Segundo Llamado...');
+      const data3 = await fetchData(JSON.parse(data2).origin.url, async data=>{
+        try {
+          return data
+        } catch (error3) {
+          console.error(`Error  ${error3}`)
+        }
+      })
+      console.log('Tercer Llamado...');
+      console.log(`Personajes: ${JSON.parse(data1).info.count}`);
+      console.log(`Primer Personaje: ${JSON.parse(data2).name}`);
+      console.log(`Dimensión: ${JSON.parse(data3).dimension}`);
+  });
+} catch (error1) {
+  console.error(`Error ${error1}`)
+}

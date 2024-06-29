c28fdbc557c4028b80f49477a07b77e02518b26d
Samir Priego <samir.adevs@gmail.com>
2024-06-29 01:13:49 -0600

    4.1
    Problema 4 mejorado, ahora el codigo es más claro, se elimina el uso del callback con una sintaxis async/await más clasra. La canidad de try/catch es utilizada para mostrar los 3 posibles errores

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import { XMLHttpRequest } from "xmlhttprequest";
 const API = 'https://rickandmortyapi.com/api/character/';
 var xhttp = new XMLHttpRequest();
 
-async function fetchData(url_api, callback){
+async function fetchData(url_api){
   return new Promise((resolve, reject)=>{
     xhttp.onreadystatechange = function (e) {
       if (xhttp.readyState === 4) {
         if (xhttp.status === 200)
-          resolve(callback(xhttp.responseText));
+          resolve(xhttp.responseText);
         else reject(new Error(url_api));
       }
       else reject(new Error(url_api));
@@ -19,30 +19,24 @@ async function fetchData(url_api, callback){
   })
 };
 
-
-try {
-  await fetchData(API, async data1=>{
-      console.log('Primer Llamado...');
-      const data2 = await fetchData(API + JSON.parse(data1).results[0].id, async data=>{
-        try {
-          return data
-        } catch (error2) {
-          console.error(`Error  ${error2}`)     
-        }
-      })
-      console.log('Segundo Llamado...');
-      const data3 = await fetchData(JSON.parse(data2).origin.url, async data=>{
-        try {
-          return data
-        } catch (error3) {
-          console.error(`Error  ${error3}`)
-        }
-      })
+try{
+  const data1 = await fetchData(API);
+  console.log('Primer Llamado...');
+  try {
+    const data2 = await fetchData(API + JSON.parse(data1).results[0].id)
+    console.log('Segundo Llamado...');
+    try {
+      const data3 = await fetchData(JSON.parse(data2).origin.url)
       console.log('Tercer Llamado...');
       console.log(`Personajes: ${JSON.parse(data1).info.count}`);
       console.log(`Primer Personaje: ${JSON.parse(data2).name}`);
       console.log(`Dimensión: ${JSON.parse(data3).dimension}`);
-  });
+    } catch (error3) {
+      console.error(`Error ${error3}`)
+    }
+  } catch (error2) {
+    console.error(`Error ${error2}`)
+  }
 } catch (error1) {
   console.error(`Error ${error1}`)
 }

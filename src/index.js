8fce18d89b9a8d2d984d550b78b791333e2f0b33
Minos <JhonatanMinos>
2024-06-30 01:19:49 -0600

    solucion al primer test

--- src/index.js
+++ src/index.js
@@ -1,33 +1,43 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var A = 'https://rickandmortyapi.com/api/character/';
-var B = new XMLHttpRequest();
+const A = "https://rickandmortyapi.com/api/character/";
+const B = new XMLHttpRequest();
 
-function X(a, b) {
-  B.onreadystatechange = function (e) {
-    if (B.readyState == '4') {
-      if (B.status === '200')
-        b(null, B.responseText);
-      else return b(a);
-    }
-    else return b(a);
-  };
-  B.open('GET', a, false);
-  B.send();
+const X = (url) => {
+  return new Promise((resolve, reject) => {
+    B.onreadystatechange = () => {
+      if (B.readyState === 4) {
+        if (B.status === 200) {
+          resolve(JSON.parse(B.responseText));
+        } else {
+          reject({ status: B.status, statusText: B.statusText });
+        }
+      }
+    };
+
+    B.open("GET", url); // Asíncrono
+    B.send();
+  });
 };
 
-X(A, function (c, d) {
-  if (c) return console.error('Error' + ' ' + c);
-  console.log('Primer Llamado...');
-  X(A + d.results[0].id, function (e, f) {
-    if (e) return console.error('Error' + ' ' + e);
-    console.log('Segundo Llamado...');
-    X(JSON.parse(f).origin.url, function (g, h) {
-      if (g) return console.error('Error' + ' ' + g);
-      console.log('Tercer Llamado...');
-      console.log('Personajes:' + ' ' + JSON.parse(d).info.count);
-      console.log('Primer Personaje:' + ' ' + JSON.parse(f).name);
-      console.log('Dimensión:' + ' ' + JSON.parse(h).dimension);
-    });
+X(A)
+  .then((responseA) => {
+    console.log("Primer Llamado...");
+    const characterId = responseA.results[0].id;
+    console.log(`Personajes: ${responseA.info.count}`);
+    return X(`${A}${characterId}`);
+  })
+  .then((responseB) => {
+    console.log("Segundo Llamado...");
+    const originUrl = responseB.origin.url;
+    console.log(`Primer Personaje: ${responseB.name}`);
+    return X(originUrl);
+  })
+  .then((responseC) => {
+    console.log("Tercer Llamado...");
+    console.log(`Dimensión: ${responseC.dimension}`);
+  })
+  .catch((error) => {
+    console.error("Error en la solicitud:", error);
   });
-});
+

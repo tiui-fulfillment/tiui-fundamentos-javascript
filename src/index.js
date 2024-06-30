a2aaf030a7bf8d5181885da19bf0c15f54d78c56
Alberto <2122200398@soy.utj.edu.mx>
2024-06-30 10:30:16 -0600

    Se realizo la solucion completa

--- src/index.js
+++ src/index.js
@@ -1,33 +1,46 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var A = 'https://rickandmortyapi.com/api/character/';
-var B = new XMLHttpRequest();
+const A = 'https://rickandmortyapi.com/api/character/';
 
-function X(a, b) {
-  B.onreadystatechange = function (e) {
-    if (B.readyState == '4') {
-      if (B.status === '200')
-        b(null, B.responseText);
-      else return b(a);
-    }
-    else return b(a);
-  };
-  B.open('GET', a, false);
-  B.send();
+const fetchData = (url) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          try {
+            const data = JSON.parse(xhr.responseText);
+            resolve(data);
+          } catch (error) {
+            reject(`Error parsing JSON response: ${error.message}`);
+          }
+        } else {
+          reject(`HTTP error: ${xhr.status}`);
+        }
+      }
+    };
+    xhr.send();
+  });
 };
 
-X(A, function (c, d) {
-  if (c) return console.error('Error' + ' ' + c);
-  console.log('Primer Llamado...');
-  X(A + d.results[0].id, function (e, f) {
-    if (e) return console.error('Error' + ' ' + e);
+const getCharacterData = async () => {
+  try {
+    console.log('Primer Llamado...');
+    const data = await fetchData(A);
+
     console.log('Segundo Llamado...');
-    X(JSON.parse(f).origin.url, function (g, h) {
-      if (g) return console.error('Error' + ' ' + g);
-      console.log('Tercer Llamado...');
-      console.log('Personajes:' + ' ' + JSON.parse(d).info.count);
-      console.log('Primer Personaje:' + ' ' + JSON.parse(f).name);
-      console.log('Dimensión:' + ' ' + JSON.parse(h).dimension);
-    });
-  });
-});
+    const characterData = await fetchData(`${A}${data.results[0].id}`);
+
+    console.log('Tercer Llamado...');
+    const originData = await fetchData(characterData.origin.url);
+
+    console.log(`Personajes: ${data.info.count}`);
+    console.log(`Primer Personaje: ${characterData.name}`);
+    console.log(`Dimensión: ${originData.dimension}`);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+};
+
+getCharacterData();

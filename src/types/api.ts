45239b5d4d0fd573f5991bb11db14d0f6e09d8a4
Alan Javier <87110072+AlanJSan@users.noreply.github.com>
2024-06-30 12:02:38 -0600

    Add typescript

---
+++ src/types/api.ts
@@ -0,0 +1,73 @@
+export interface Character {
+    id:       number;
+    name:     string;
+    status:   string;
+    species:  string;
+    type:     string;
+    gender:   string;
+    origin:   Location;
+    location: Location;
+    image:    string;
+    episode:  string[];
+    url:      string;
+    created:  Date;
+}
+
+export interface CharactersResponse {
+    info:    Info;
+    results: Result[];
+}
+
+export interface CharacterOrigin {
+    id:        number;
+    name:      string;
+    type:      string;
+    dimension: string;
+    residents: string[];
+    url:       string;
+    created:   Date;
+}
+
+export interface Info {
+    count: number;
+    pages: number;
+    next:  string;
+    prev:  null;
+}
+
+export interface Result {
+    id:       number;
+    name:     string;
+    status:   Status;
+    species:  Species;
+    type:     string;
+    gender:   Gender;
+    origin:   Location;
+    location: Location;
+    image:    string;
+    episode:  string[];
+    url:      string;
+    created:  Date;
+}
+
+export enum Gender {
+    Female = "Female",
+    Male = "Male",
+    Unknown = "unknown",
+}
+
+export interface Location {
+    name: string;
+    url:  string;
+}
+
+export enum Species {
+    Alien = "Alien",
+    Human = "Human",
+}
+
+export enum Status {
+    Alive = "Alive",
+    Dead = "Dead",
+    Unknown = "unknown",
+}
